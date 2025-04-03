b8a44a000224abf39fd52d753f6e7fe1f1cd9089
tranduyen77 <31897486+tranduyen77@users.noreply.github.com>
2025-04-03 11:01:45 +0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/qr_inventory/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/qr_inventory/MainActivity.kt
@@ -1,6 +1,7 @@
-﻿package com.example.qr_inventory
+package com.example.qr_inventory
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    // Không cần thêm bất kỳ code nào nếu không có custom logic
 }
